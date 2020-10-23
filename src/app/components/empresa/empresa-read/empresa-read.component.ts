0abf9945c9fd05f74b97cdf7713e81e4055b9686
Alberto Feitoza <alberto.feitoza@pixeon.com>
2020-10-23 17:02:18 -0300

    Correção na VIew empresa

--- src/app/components/empresa/empresa-read/empresa-read.component.ts
+++ src/app/components/empresa/empresa-read/empresa-read.component.ts
@@ -3,6 +3,7 @@ import { Endereco } from './../../../models/endereco/modelEndereco';
 import { Contato } from './../../../models/contato/modelContato';
 import { Empresa } from './../../../models/empresa/ModelEmpresa';
 import { Component, OnInit } from '@angular/core';
+import { elementAt } from 'rxjs/operators';
 
 @Component({
   selector: 'app-empresa-read',
@@ -13,6 +14,7 @@ export class EmpresaReadComponent implements OnInit {
 
   
   empresa: Empresa[];
+  empresas: Empresa[];
   contato : Contato;
   endereco : Endereco;
   displayedColumns = ['id','cnpj', 'razaoSocial','nomeFantasia','inscricaoEstadual','inscricaoMunicipal','action']  
@@ -26,12 +28,43 @@ export class EmpresaReadComponent implements OnInit {
 
   }
 
-  buscarEmpresa(): void {
+  buscarEmpresa(): void 
+  {
     const tipo = `${"/Empresa"}`
     this.serviceEmpresa.read(tipo).subscribe(empresa => {
       this.empresa = empresa;
+     
+      
+     
+      let empId = Number(localStorage.getItem("empId"));
+      let grpId = Number(localStorage.getItem("grpUs"));
+    
+        this.empresas = this.empresa;
+        this.empresa =  new Array();
 
-  })
+          if(grpId == 1)
+          {
+            this.empresas.forEach(element => {
+            this.empresa.push(element)
+             });
+          }
+          else
+          {
+            for (let index = 0; index < this.empresas.length; index++) {
+              const element = this.empresas[index];
+              if (element.empresaPai == empId)
+                this.empresa.push(element)
+            }
+
+          }
+    })
   }
-
 }
+  
+
+
+      
+
+
+
+
