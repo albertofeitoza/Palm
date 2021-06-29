cf10738d1925e6e2b8086865b54656b4281ec107
Alberto Feitoza <alberto.feitoza@pixeon.com>
2021-06-29 10:46:01 -0300

    Criação da Read Agenda

--- src/app/Aplicacoes/agenda/agenda-read/agenda-read.component.ts
+++ src/app/Aplicacoes/agenda/agenda-read/agenda-read.component.ts
@@ -3,6 +3,9 @@ import { Componentes } from './../../../Negocio/Componentes';
 import { Aplicacao } from './../../../Negocio/Aplicacao';
 import { UtilService } from './../../../services/util.service';
 import { Component, OnInit } from '@angular/core';
+import { Agenda } from 'src/app/models/Agenda/modelAgenda';
+import { ServiceAllService } from 'src/app/services/service-all.service';
+import { Endpoint } from 'src/app/Negocio/Endpoint';
 
 
 @Component({
@@ -12,23 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgendaReadComponent implements OnInit {
 
-
-  carregaComponente : string
-
+  Colunas = ['id','DtCriacao', 'nomeAgenda','empresaid','unidadeid','salaid','profissionalid',
+                      'substituicao','grupoAgendaid','vigenciaInico','vigenciaFim','considerarFeriado','ativo','action']  
+  agenda : Agenda[];
 
   constructor(private UtilService : UtilService,
-              private route : Router
+              private route : Router,
+              private _repAgenda : ServiceAllService<Agenda>
              ) { }
 
   ngOnInit(): void {
     this.UtilService.AtualizarMenu(Aplicacao.Agenda,'app_registration','')
   }
 
-  RotaInterna(rota : string){
-    this.carregaComponente = rota;
-    this.route.navigate(['Agenda'])   
-    
  
+  buscarAgenda(){
+    
+    this._repAgenda.read(Endpoint.Agenda).subscribe(a => {
+      this.agenda = a;
+    });
+
+
   }
 
+
+  navigateToAgendaCreate(){
+    this.route.navigate(['agenda/create'])
+  }
 }
