31d2738b56cfb6573b57cdc90892ffd5a9b4ab00
Alberto Feitoza <alberto.feitoza@outlook.com>
2021-12-01 09:15:07 -0300

    Correção serviço Atualização

--- atualizaProjeto.bat
+++ atualizaProjeto.bat
@@ -2,15 +2,27 @@
 
 for /f %%a in ('powershell Invoke-RestMethod https://ipinfo.io/ip') do set PublicIP=%%a
 
-echo const PROXY_CONFIG = [> proxy.config_novoIP.js
-echo     {>> proxy.config_novoIP.js
-echo       context: '/api',>> proxy.config_novoIP.js
-echo       target: 'http://%PublicIP%:888',>> proxy.config_novoIP.js
-echo       secure : false,>> proxy.config_novoIP.js
-echo       logLevel : 'debug',>> proxy.config_novoIP.js
-echo     }>> proxy.config_novoIP.js
-echo   ];>> proxy.config_novoIP.js
-echo   module.exports = PROXY_CONFIG;>> proxy.config_novoIP.js
-pause
+FIND /i "%PublicIP%" "proxy.config.js"
 
+if NOT %ERRORLEVEL% EQU 0 ( 
 
+git pull
+  
+echo const PROXY_CONFIG = [> proxy.config.js
+echo     {>> proxy.config.js
+echo       context: '/api',>> proxy.config.js
+echo       target: 'http://%PublicIP%:888',>> proxy.config.js
+echo       secure : false,>> proxy.config.js
+echo       logLevel : 'debug',>> proxy.config.js
+echo     }>> proxy.config.js
+echo   ];>> proxy.config.js
+echo   module.exports = PROXY_CONFIG;>> proxy.config.js
+
+DEL atualizaProjeto.bat.bak
+DEL proxy.config.js.bak
+
+git add proxy.config.js
+git commit -m "Atualização do IP Proxy"
+git push origin master
+  
+)
