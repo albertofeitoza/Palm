f5359bf02d9b73f0815581c3750e2da6318ef3e3
Alberto Feitoza <alberto.feitoza@outlook.com>
2022-04-11 13:59:45 -0300

    Atualiza├º├úo do IP Proxy

--- proxy.config.js
+++ proxy.config.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
     {
       context: '/api',
-      target: 'http://localhost:887',
+      target: 'http://177.115.9.143:888',
       secure : false,
       logLevel : 'debug',
     }
